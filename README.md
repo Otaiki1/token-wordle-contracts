0e6d920143370f7379efc70a718d947fa3663107
Abdulsamad sadiq <otaikisadiq2804@gmail.com>
2023-06-09 02:25:41 +0100

    feat: add deploy scripts to mumbai

--- README.md
+++ README.md
@@ -1,13 +1,7 @@
-# Sample Hardhat Project
+# Token Wordle Contracts
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
-
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.js
-```
+STAKING TOKEN HAS BEEN DEPLOYED TO ________ 0x431b2b148EE63DFdc7D0e73df389A7865EccBfe5
+STAKING CONTRACT HAS BEEN DEPLOYED TO ________ 0x46f6391f0625f33a10a63875bBb6577B952411c2
+GAME CONTRACT HAS BEEN DEPLOYED TO ________ 0x6039efb5080fE3f652F7ACE3C81313f3F00BB987
+SUCCESSFULLY SENT 95 PERCENT , TXN HASH IS _____ 0xa9d032e5e21752dfffb02558a5f9bb4b035ffd61c98c47a9b6b80c857772c73d
+vrfd20Contract was successfully deployed to _______ 0x0A3478083407Fa46bA842d066b7a4A80a710dd43
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,24 +4,96 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
+//vrfCoordinatorFamtom = 0xbd13f08b8352A3635218ab9418E340c60d6Eb418
+//keyhash = 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c
 const hre = require("hardhat");
+require("dotenv").config();
+
+const PERCENT = 1;
+const REWARD_AMOUNT = 1;
+const gameSecretKey = ethers.utils.formatBytes32String(
+  process.env.GAME_SECRET_KEY
+);
+
+const VRF_COORDINATOR_ADDRESS = process.env.MUMBAI_VRF_COORDINATOR;
+const KEYHASH = process.env.MUMBAI_KEY_HASH;
+const SUBSCRIPTION_ID = process.env.SUBSCRIPTION_ID;
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const StakingToken = await ethers.getContractFactory("TLCToken");
+  const stakingToken = await StakingToken.deploy();
+  console.log(
+    "STAKING TOKEN HAS BEEN DEPLOYED TO ________",
+    stakingToken.address
+  );
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  const StakingContract = await ethers.getContractFactory("Staking");
+  const stakingContract = await StakingContract.deploy(stakingToken.address);
+  console.log(
+    "STAKING CONTRACT HAS BEEN DEPLOYED TO ________",
+    stakingContract.address
+  );
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const GameContract = await ethers.getContractFactory("GameContract");
+  const gameContract = await GameContract.deploy(
+    PERCENT,
+    REWARD_AMOUNT,
+    stakingToken.address,
+    stakingContract.address,
+    gameSecretKey
+  );
+  console.log(
+    "GAME CONTRACT HAS BEEN DEPLOYED TO ________",
+    gameContract.address
+  );
 
-  await lock.deployed();
+  //we also want to give the game contract 95% of the token
+  const send95PercentTo = await stakingToken.send95PercentTo(
+    gameContract.address
+  );
+  console.log(
+    "SUCCESSFULLY SENT 95 PERCENT , TXN HASH IS _____",
+    send95PercentTo.hash
+  );
+
+  //optional : deployssss , deploy mock coordinator
+
+  // const VrfCoordinatorV2Mock = await ethers.getContractFactory(
+  //   "VRFCoordinatorV2Mock"
+  // );
+  // const vrfCoordinatorV2Mock = await VrfCoordinatorV2Mock.deploy(
+  //   BASE_FEE,
+  //   GAS_PRICE_LINK
+  // );
+  // //CREATE SUBSCRIPTION HERE
+  // const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
+  // const transactionReceipt = await transactionResponse.wait(1);
+  // const subscriptionId = transactionReceipt.events[0].args.subId;
+
+  // console.log("SuBSCription ID is __---", subscriptionId);
+
+  // await vrfCoordinatorV2Mock.fundSubscription(
+  //   subscriptionId,
+  //   VRF_SUB_FUND_AMOUNT
+  // );
+
+  const VRFD20Contract = await ethers.getContractFactory("VRFD20");
+  const vrfd20Contract = await VRFD20Contract.deploy(
+    SUBSCRIPTION_ID,
+    gameSecretKey,
+    VRF_COORDINATOR_ADDRESS,
+    KEYHASH
+  );
 
   console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    "vrfd20Contract was successfully deployed to _______",
+    vrfd20Contract.address
   );
+
+  // await vrfCoordinatorV2Mock.addConsumer(
+  //   subscriptionId,
+  //   vrfd20Contract.address
+  // );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
